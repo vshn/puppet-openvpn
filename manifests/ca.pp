b38be611c33b5980087f43b68692b453015a4d69
Nicolas Bigler <nicolas.bigler@vshn.ch>
2019-12-13 17:08:26 +0100

    Replace deprecated source_permissions parameter
    The parameter has been deprecated by Puppetlabs and will be removed in
    Puppet6. In order to ensure that the module keeps working in the future
    the functionality of the parameter had to be replaced.
    
    The only easy way to solve this, is by using an exec statement that uses
    rsync to sync the files from the source folder

--- manifests/ca.pp
+++ manifests/ca.pp
@@ -82,21 +82,17 @@ define openvpn::ca (
       mode   => '0750'
   })
 
-  file { "${server_directory}/${name}/easy-rsa" :
-    ensure             => directory,
-    recurse            => true,
-    links              => 'follow',
-    source_permissions => 'use',
-    group              => 0,
-    source             => "file:${openvpn::easyrsa_source}",
-    require            => File["${server_directory}/${name}"],
+  exec { "init easy-rsa ${name}":
+    command  => "rsync -a --exclude vars ${openvpn::easyrsa_source} ${server_directory}/${name}/easy-rsa",
+    provider => "shell",
+    require  => File["${server_directory}/${name}"],
   }
 
   file { "${server_directory}/${name}/easy-rsa/revoked":
     ensure  => directory,
     mode    => '0750',
     recurse => true,
-    require => File["${server_directory}/${name}/easy-rsa"],
+    require => Exec["init easy-rsa ${name}"],
   }
 
   if $facts['os']['family'] == 'Archlinux' {
@@ -133,7 +129,7 @@ define openvpn::ca (
             'key_ou'           => $key_ou,
           }
         ),
-        require => File["${server_directory}/${name}/easy-rsa"],
+        require => Exec["init easy-rsa ${name}"],
       }
 
       if $openvpn::link_openssl_cnf {
@@ -235,14 +231,14 @@ define openvpn::ca (
   }
 
   file { "${server_directory}/${name}/easy-rsa/openssl.cnf":
-    require => File["${server_directory}/${name}/easy-rsa"],
+    require => Exec["init easy-rsa ${name}"],
   }
 
   file { "${server_directory}/${name}/keys":
     ensure  => link,
     target  => "${server_directory}/${name}/easy-rsa/keys",
     mode    => '0640',
-    require => File["${server_directory}/${name}/easy-rsa"],
+    require => Exec["init easy-rsa ${name}"],
   }
 
   file { "${server_directory}/${name}/crl.pem":
